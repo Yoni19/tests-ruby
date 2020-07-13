ba1743e77f32a43cf401e3db89c05ce2175b82d0
Yoni19 <anthony.krief@hotmail.fr>
2020-07-13 18:52:58 +0200

    update exo

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -1,13 +1,3 @@
-def time_string(num)
-    seconds = num
-    minutes = (seconds / 60) % 60 
-    hours = seconds / (60 * 60)
-    seconds = seconds.to_i
-    minutes = minutes.to_i
-    hours = hours.to_i
-    if seconds < 10 & minutes < 10 & hours < 10
-    print "0#{hours}:0#{minutes}:0#{seconds}"
-    end
+def time_string(seconds)
+    return Time.at(seconds).utc.strftime("%H:%M:%S")
 end
-
-time_string(60)
